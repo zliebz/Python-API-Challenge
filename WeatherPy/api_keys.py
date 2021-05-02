11bbeb167cf212d8106bc28c23fe054c4077cdc4
zliebz <zachary.liebler@gmail.com>
2021-05-01 21:24:54 -0500

    Final Push

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "f033023d94527777ec9eb14606b16100"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyB9c7ueh-7In-wlLh8QZdkvNMbPftT5Zfc"
